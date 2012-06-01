19950f37233d75c428ffd909fbeea0c219332d1a
Christof <christofschlaak@web.de>
2012-06-01 17:12:09 +0200

    SlidePanel

---
+++ src/com/slide/test/MenuActivity.java
@@ -0,0 +1,36 @@
+package com.slide.test;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.KeyEvent;
+
+
+public class MenuActivity extends FragmentActivity{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    mSlideoutHelper = new SlideoutHelper(this);
+	    mSlideoutHelper.activate();
+	    getSupportFragmentManager().beginTransaction().add(R.id.slideout_placeholder, new MenuFragment(), "menu").commit();
+	    mSlideoutHelper.open();
+	}
+
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if(keyCode == KeyEvent.KEYCODE_BACK){
+			mSlideoutHelper.close();
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+
+	public SlideoutHelper getSlideoutHelper(){
+		return mSlideoutHelper;
+	}
+	
+	private SlideoutHelper mSlideoutHelper;
+
+}
---
+++ src/com/slide/test/SlideoutActivity.java
@@ -0,0 +1,31 @@
+package com.slide.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+
+public class SlideoutActivity extends Activity {
+
+	private SlideoutHelper mSlideoutHelper;
+	
+	public static void prepare(Activity activity, int id, int width){
+		SlideoutHelper.prepare(activity, id, width);
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    mSlideoutHelper.activate();
+	    mSlideoutHelper.open();
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if(keyCode == KeyEvent.KEYCODE_BACK){
+			mSlideoutHelper.close();
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+	
+}
