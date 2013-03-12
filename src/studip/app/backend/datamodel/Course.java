7179c089d1c7a3fa3cbf906893fe17153cfde279
Jörn <j.domnik@googlemail.com>
2013-03-12 16:53:54 +0100

    parse students, teachers, tutors and modules into object

--- src/studip/app/backend/datamodel/Course.java
+++ src/studip/app/backend/datamodel/Course.java
@@ -10,57 +10,151 @@
  */
 package studip.app.backend.datamodel;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import studip.app.backend.db.CoursesContract;
+import android.database.Cursor;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 /**
  * @author joern
  * 
  */
 
 public class Course {
+    public String course_id;
+    public Long start_time;
+    public Long duration_time;
+    public Double number;
+    public String title;
+    public String subtitle;
+    public String type;
+    public Modules modules;
+    public String description;
+    public String location;
+    public String semester_id;
+    public ArrayList<String> teachers;
+    public ArrayList<String> tutors;
+    public ArrayList<String> students;
+    public String colors;
+
     public Course() {
     }
 
-    public Course(String course_id, String start_time, String duration_time,
-	    String title, String subtitle, String description, String location,
-	    String type, String semesert_id) {
+    /**
+     * @param course_id
+     * @param start_time
+     * @param duration_time
+     * @param number
+     * @param title
+     * @param subtitle
+     * @param type
+     * @param modules
+     * @param description
+     * @param location
+     * @param semester_id
+     * @param teachers
+     * @param tutors
+     * @param students
+     * @param colors
+     */
+    public Course(String course_id, Long start_time, Long duration_time,
+	    Double number, String title, String subtitle, String type,
+	    Modules modules, String description, String location,
+	    String semester_id, ArrayList<String> teachers,
+	    ArrayList<String> tutors, ArrayList<String> students, String colors) {
 	this.course_id = course_id;
 	this.start_time = start_time;
 	this.duration_time = duration_time;
+	this.number = number;
 	this.title = title;
 	this.subtitle = subtitle;
+	this.type = type;
+	this.modules = modules;
 	this.description = description;
 	this.location = location;
-	this.type = type;
-	this.semester_id = semesert_id;
+	this.semester_id = semester_id;
+	this.teachers = teachers;
+	this.tutors = tutors;
+	this.students = students;
+	this.colors = colors;
     }
 
-    // public static class Modules {
-    // public Boolean calendar;
-    // public Boolean chat;
-    // public Boolean documents;
-    // public Boolean documents_folder_permissions;
-    // public Boolean elearning_interface;
-    // public Boolean forum;
-    // public Boolean literature;
-    // public Boolean participants;
-    // public Boolean personal;
-    // public Boolean schedule;
-    // public Boolean scm;
-    // public Boolean wiki;
-    // }
-    // TODO Feststellen welche Entities wirklich gebraucht werden.
-    public String course_id;
-    public String start_time;
-    public String duration_time;
-    public String title;
-    public String subtitle;
-    public String description;
-    public String location;
-    public String type;
-    public String semester_id;
-    // public ArrayList<String> teachers;
-    // public ArrayList<String> tutors;
-    // public ArrayList<String> students;
-    // public Modules modules;
-    // public String colors;
+    public Course(Cursor cursor) {
+	ObjectMapper mMapper = new ObjectMapper();
+	ArrayList<String> students = null;
+	ArrayList<String> tutors = null;
+	ArrayList<String> teachers = null;
+	Modules modules = null;
+
+	try {
+	    students = mMapper.readValue(cursor.getString(cursor
+		    .getColumnIndex(CoursesContract.Columns.COURSE_STUDENTS)),
+		    ArrayList.class);
+	    tutors = mMapper.readValue(cursor.getString(cursor
+		    .getColumnIndex(CoursesContract.Columns.COURSE_STUDENTS)),
+		    ArrayList.class);
+	    teachers = mMapper.readValue(cursor.getString(cursor
+		    .getColumnIndex(CoursesContract.Columns.COURSE_STUDENTS)),
+		    ArrayList.class);
+	    modules = mMapper.readValue(cursor.getString(cursor
+		    .getColumnIndex(CoursesContract.Columns.COURSE_MODULES)),
+		    Modules.class);
+
+	} catch (JsonParseException e) {
+	    e.printStackTrace();
+	} catch (JsonMappingException e) {
+	    e.printStackTrace();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+
+	this.course_id = cursor.getString(cursor
+		.getColumnIndex(CoursesContract.Columns.COURSE_ID));
+	this.start_time = cursor.getLong(cursor
+		.getColumnIndex(CoursesContract.Columns.COURSE_START_TIME));
+	this.duration_time = cursor.getLong(cursor
+		.getColumnIndex(CoursesContract.Columns.COURSE_DURATION_TIME));
+	this.number = cursor.getDouble(cursor
+		.getColumnIndex(CoursesContract.Columns.COURSE_NUMBER));
+	this.title = cursor.getString(cursor
+		.getColumnIndex(CoursesContract.Columns.COURSE_TITLE));
+	this.subtitle = cursor.getString(cursor
+		.getColumnIndex(CoursesContract.Columns.COURSE_SUBTITLE));
+	this.type = cursor.getString(cursor
+		.getColumnIndex(CoursesContract.Columns.COURSE_TYPE));
+	this.modules = modules;
+	this.description = cursor.getString(cursor
+		.getColumnIndex(CoursesContract.Columns.COURSE_DESCIPTION));
+	this.location = cursor.getString(cursor
+		.getColumnIndex(CoursesContract.Columns.COURSE_LOCATION));
+	this.semester_id = cursor.getString(cursor
+		.getColumnIndex(CoursesContract.Columns.COURSE_SEMESERT_ID));
+	this.teachers = teachers;
+	this.tutors = tutors;
+	this.students = students;
+	this.colors = cursor.getString(cursor
+		.getColumnIndex(CoursesContract.Columns.COURSE_COLORS));
+
+    }
+
+    public static class Modules {
+	public Boolean calendar = false;
+	public Boolean chat = false;
+	public Boolean documents = false;
+	public Boolean documents_folder_permissions = false;
+	public Boolean elearning_interface = false;
+	public Boolean forum = false;
+	public Boolean literature = false;
+	public Boolean participants = false;
+	public Boolean personal = false;
+	public Boolean schedule = false;
+	public Boolean scm = false;
+	public Boolean wiki = false;
+    }
 
 }
