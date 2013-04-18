51744d24e171d5c5b02151b6bcad21af255151ba
Jörn <j.domnik@googlemail.com>
2013-04-18 17:46:53 +0200

    server chooser is now a fragment

--- src/de/elanev/studip/android/app/backend/net/ChooseServerActivity.java
+++ src/de/elanev/studip/android/app/backend/net/ChooseServerActivity.java
@@ -7,15 +7,10 @@
  ******************************************************************************/
 package de.elanev.studip.android.app.backend.net;
 
-import java.util.ArrayList;
-
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import de.elanev.studip.android.app.R;
-import de.elanev.studip.android.app.frontend.util.ArrayAdapterItem;
 import de.elanev.studip.android.app.frontend.util.BaseSlidingFragmentActivity;
-import de.elanev.studip.android.app.frontend.util.GeneralListFragment;
-import de.elanev.studip.android.app.frontend.util.TextItem;
-import de.elanev.studip.android.app.util.TempServerDeclares;
 
 /**
  * 
@@ -36,25 +31,18 @@ public class ChooseServerActivity extends BaseSlidingFragmentActivity {
 		super.onCreate(savedInstanceState);
 		setTitle(R.string.choose_server);
 		this.setContentView(R.layout.content_frame);
-		GeneralListFragment glf = new GeneralListFragment();
-		glf.itemList = this.getItems();
-		getSupportFragmentManager().beginTransaction()
-				.replace(R.id.content_frame, glf, "glf").commit();
+		FragmentManager fm = getSupportFragmentManager();
+		ChooseServerFragment frag = null;
+		if (savedInstanceState == null) {
+			frag = (ChooseServerFragment) ChooseServerFragment.instantiate(
+					this, ChooseServerFragment.class.getName());
+		} else {
+			frag = (ChooseServerFragment) fm
+					.findFragmentByTag(ChooseServerFragment.class.getName());
+		}
+		fm.beginTransaction()
+				.add(R.id.content_frame, frag,
+						ChooseServerFragment.class.getName()).commit();
 	}
 
-	public ArrayList<ArrayAdapterItem> getItems() {
-
-		ArrayList<ArrayAdapterItem> items = new ArrayList<ArrayAdapterItem>();
-
-		/*
-		 * WARNING: you need your own TempServerDeclares Class in the
-		 * de.elanev.studip.android.app.util package see:
-		 * de.elanev.studip.android.app.util.TempServerDeclaresExample
-		 */
-		items.add(new TextItem(getString(R.string.Development)));
-		items.add(new ServerItem(this, TempServerDeclares.neueTestumgebung));
-		items.add(new ServerItem(this, TempServerDeclares.alteTestumgebung));
-
-		return items;
-	}
 }
---
+++ src/de/elanev/studip/android/app/backend/net/ChooseServerFragment.java
@@ -0,0 +1,139 @@
+/*******************************************************************************
+ * Copyright (c) 2013 ELAN e.V.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ ******************************************************************************/
+package de.elanev.studip.android.app.backend.net;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.actionbarsherlock.app.SherlockListFragment;
+
+import de.elanev.studip.android.app.R;
+import de.elanev.studip.android.app.util.Prefs;
+import de.elanev.studip.android.app.util.TempServerDeclares;
+
+/**
+ * @author joern
+ * 
+ */
+public class ChooseServerFragment extends SherlockListFragment {
+	private Context mContext;
+	private ArrayAdapter<Server> mAdapter;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.support.v4.app.Fragment#onCreate(android.os.Bundle)
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mContext = getActivity();
+		mAdapter = new ServerAdapter(mContext, R.layout.list_item_image_text,
+				getItems());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.support.v4.app.Fragment#onActivityCreated(android.os.Bundle)
+	 */
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		setListAdapter(mAdapter);
+		mAdapter.notifyDataSetChanged();
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * android.support.v4.app.ListFragment#onListItemClick(android.widget.ListView
+	 * , android.view.View, int, long)
+	 */
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		Server item = (Server) v.getTag();
+		if (item != null) {
+			Prefs.getInstance(getActivity().getApplicationContext()).setServer(
+					item);
+			((Activity) mContext).finish();
+		}
+	}
+
+	public Server[] getItems() {
+		/*
+		 * WARNING: you need your own TempServerDeclares Class in the
+		 * de.elanev.studip.android.app.util package see:
+		 * de.elanev.studip.android.app.util.TempServerDeclaresExample
+		 */
+		Server[] items = new Server[] { TempServerDeclares.neueTestumgebung,
+				TempServerDeclares.alteTestumgebung };
+		return items;
+	}
+
+	private class ServerAdapter extends ArrayAdapter<Server> {
+		private Context context;
+		private int textViewResourceId;
+		private Server[] data = null;
+
+		/**
+		 * @param context
+		 * @param textViewResourceId
+		 */
+		public ServerAdapter(Context context, int textViewResourceId,
+				Server[] data) {
+			super(context, textViewResourceId);
+			this.context = context;
+			this.textViewResourceId = textViewResourceId;
+			this.data = data;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see android.widget.ArrayAdapter#getView(int, android.view.View,
+		 * android.view.ViewGroup)
+		 */
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+
+			if (convertView == null) {
+				LayoutInflater inflater = ((Activity) context)
+						.getLayoutInflater();
+				convertView = inflater.inflate(textViewResourceId, parent,
+						false);
+
+			}
+			Server server = data[position];
+			((TextView) convertView.findViewById(R.id.text))
+					.setText(server.NAME);
+			convertView.setTag(server);
+			return convertView;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see android.widget.ArrayAdapter#getCount()
+		 */
+		@Override
+		public int getCount() {
+			return data.length;
+		}
+	}
+}
