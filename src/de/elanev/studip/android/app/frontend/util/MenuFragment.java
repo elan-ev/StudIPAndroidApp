c86fd38f0a9fe2b078728e31e5a2b09b8fd06051
Jörn <j.domnik@googlemail.com>
2013-04-23 19:35:12 +0200

    Fixed wrong menu item selection when not logged in

--- src/de/elanev/studip/android/app/frontend/util/MenuFragment.java
+++ src/de/elanev/studip/android/app/frontend/util/MenuFragment.java
@@ -11,6 +11,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,6 +24,7 @@ import com.slidingmenu.lib.app.SlidingFragmentActivity;
 
 import de.elanev.studip.android.app.R;
 import de.elanev.studip.android.app.frontend.courses.CoursesActivity;
+import de.elanev.studip.android.app.frontend.messages.MessagesActivity;
 import de.elanev.studip.android.app.frontend.news.NewsViewActivity;
 import de.elanev.studip.android.app.util.Prefs;
 
@@ -32,7 +34,18 @@ import de.elanev.studip.android.app.util.Prefs;
  */
 public class MenuFragment extends ListFragment {
 
+	public static final String TAG = MenuFragment.class.getSimpleName();
 	private static final String ACTIVE_ITEM = "activeItem";
+	// Menu Items
+	private static final int NEWS_MENU_ITEM = 0;
+	private static final int COURSES_MENU_ITEM = 1;
+	private static final int MESSAGES_MENU_ITEM = 2;
+	private static final int CONTACTS_MENU_ITEM = 3;
+	private static final int SETTINGS_MENU_ITEM = 4;
+	private static final int HELP_MENU_ITEM = 5;
+	private static final int INFO_MENU_ITEM = 6;
+	private static final int LOGOUT_MENU_ITEM = 7;
+
 	private Context mContext;
 
 	/*
@@ -65,22 +78,24 @@ public class MenuFragment extends ListFragment {
 		getView().setBackgroundColor(getResources().getColor(R.color.dark));
 		MenuAdapter adapter = new MenuAdapter(getActivity());
 		if (Prefs.getInstance(mContext).isAppAuthorized()) {
-			adapter.add(new MenuItem(R.drawable.news, getString(R.string.News)));
+			adapter.add(new MenuItem(R.drawable.news, getString(R.string.News),
+					NEWS_MENU_ITEM));
 			adapter.add(new MenuItem(R.drawable.seminar,
-					getString(R.string.Courses)));
+					getString(R.string.Courses), COURSES_MENU_ITEM));
 			adapter.add(new MenuItem(R.drawable.mail,
-					getString(R.string.Messages)));
+					getString(R.string.Messages), MESSAGES_MENU_ITEM));
 			adapter.add(new MenuItem(R.drawable.community,
-					getString(R.string.Contacts)));
+					getString(R.string.Contacts), CONTACTS_MENU_ITEM));
 		}
-		adapter.add(new MenuItem(R.drawable.admin, getString(R.string.Settings)));
+		adapter.add(new MenuItem(R.drawable.admin,
+				getString(R.string.Settings), SETTINGS_MENU_ITEM));
 		adapter.add(new MenuItem(R.drawable.question_circle,
-				getString(R.string.Help)));
+				getString(R.string.Help), HELP_MENU_ITEM));
 		adapter.add(new MenuItem(R.drawable.info_circle,
-				getString(R.string.Information)));
+				getString(R.string.Information), INFO_MENU_ITEM));
 		if (Prefs.getInstance(mContext).isAppAuthorized()) {
 			adapter.add(new MenuItem(android.R.drawable.ic_menu_revert,
-					getString(R.string.Logout)));
+					getString(R.string.Logout), LOGOUT_MENU_ITEM));
 		}
 		setListAdapter(adapter);
 		if (savedInstanceState != null) {
@@ -95,19 +110,39 @@ public class MenuFragment extends ListFragment {
 	@Override
 	public void onListItemClick(ListView lv, View v, int position, long id) {
 		Class<?> cls = null;
-		switch (position) {
-		case 0:
-			cls = NewsViewActivity.class;
-
-			break;
-		case 1:
-			cls = CoursesActivity.class;
-			break;
-		}
-		if (cls != null) {
-			((SlidingFragmentActivity) mContext).getSlidingMenu()
-					.setSelectedView(v);
-			switchActivity(cls);
+		MenuItem item = (MenuItem) lv.getItemAtPosition(position);
+		if (item != null) {
+			switch (item.itemType) {
+			case NEWS_MENU_ITEM:
+				cls = NewsViewActivity.class;
+				break;
+			case COURSES_MENU_ITEM:
+				cls = CoursesActivity.class;
+				break;
+			case MESSAGES_MENU_ITEM:
+				cls = MessagesActivity.class;
+				break;
+			case CONTACTS_MENU_ITEM:
+				Log.i(TAG, "Contacts selected");
+				break;
+			case SETTINGS_MENU_ITEM:
+				Log.i(TAG, "Settings selected");
+				break;
+			case HELP_MENU_ITEM:
+				Log.i(TAG, "Help selected");
+				break;
+			case INFO_MENU_ITEM:
+				Log.i(TAG, "Info selected");
+				break;
+			case LOGOUT_MENU_ITEM:
+				Log.i(TAG, "Logout selected");
+				break;
+			}
+			if (cls != null) {
+				((SlidingFragmentActivity) mContext).getSlidingMenu()
+						.setSelectedView(v);
+				switchActivity(cls);
+			}
 		}
 	}
 
@@ -125,10 +160,12 @@ public class MenuFragment extends ListFragment {
 	private class MenuItem {
 		public String tag;
 		public int iconRes;
+		public int itemType;
 
-		public MenuItem(int iconRes, String tag) {
+		public MenuItem(int iconRes, String tag, int itemType) {
 			this.tag = tag;
 			this.iconRes = iconRes;
+			this.itemType = itemType;
 		}
 	}
 
