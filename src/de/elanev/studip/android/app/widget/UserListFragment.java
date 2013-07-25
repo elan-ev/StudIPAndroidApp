d50b178f1f4478d67d7f9e94c5be0f5b1008a756
Jörn <j.domnik@googlemail.com>
2013-07-25 18:14:04 +0200

    Added new UserListFragment implementation and deactivated splitActionBarWhenNarrow

--- src/de/elanev/studip/android/app/widget/UserListFragment.java
+++ src/de/elanev/studip/android/app/widget/UserListFragment.java
@@ -7,14 +7,22 @@
  ******************************************************************************/
 package de.elanev.studip.android.app.widget;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import oauth.signpost.exception.OAuthCommunicationException;
 import oauth.signpost.exception.OAuthExpectationFailedException;
 import oauth.signpost.exception.OAuthMessageSignerException;
-import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.DialogInterface.OnMultiChoiceClickListener;
+import android.content.Intent;
 import android.content.OperationApplicationException;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -24,6 +32,7 @@ import android.support.v4.app.LoaderManager.LoaderCallbacks;
 import android.support.v4.content.CursorLoader;
 import android.text.TextUtils;
 import android.util.Log;
+import android.util.Pair;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.LayoutInflater;
@@ -31,7 +40,9 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -43,12 +54,14 @@ import com.android.volley.VolleyError;
 
 import de.elanev.studip.android.app.R;
 import de.elanev.studip.android.app.backend.datamodel.ContactGroups;
+import de.elanev.studip.android.app.backend.datamodel.Contacts;
 import de.elanev.studip.android.app.backend.db.AbstractContract;
 import de.elanev.studip.android.app.backend.db.ContactsContract;
 import de.elanev.studip.android.app.backend.db.UsersContract;
 import de.elanev.studip.android.app.backend.net.api.ApiEndpoints;
 import de.elanev.studip.android.app.backend.net.oauth.VolleyOAuthConsumer;
 import de.elanev.studip.android.app.backend.net.sync.ContactGroupHandler;
+import de.elanev.studip.android.app.backend.net.sync.ContactsHandler;
 import de.elanev.studip.android.app.backend.net.util.JacksonRequest;
 import de.elanev.studip.android.app.backend.net.util.StringRequest;
 import de.elanev.studip.android.app.util.Prefs;
@@ -62,10 +75,11 @@ public abstract class UserListFragment extends SherlockListFragment implements
 		LoaderCallbacks<Cursor> {
 
 	private static final String TAG = UserListFragment.class.getCanonicalName();
-	protected Context mContext;
+	protected static Context mContext;
 	protected String mFavoriteGroupId;
-	protected int mFavoriteGroupIntId;
-	private VolleyOAuthConsumer mConsumer;
+	protected static int mFavoriteGroupIntId;
+	private static VolleyOAuthConsumer mConsumer;
+	private static ContentResolver resolver;
 
 	/*
 	 * (non-Javadoc)
@@ -76,11 +90,13 @@ public abstract class UserListFragment extends SherlockListFragment implements
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		mContext = getActivity();
+		resolver = mContext.getContentResolver();
 
 		// FIXME Only temp, later use Consumer Singleton
 		mConsumer = new VolleyOAuthConsumer(Prefs.getInstance(mContext)
 				.getServer().CONSUMER_KEY, Prefs.getInstance(mContext)
 				.getServer().CONSUMER_SECRET);
+
 		mConsumer.setTokenWithSecret(Prefs.getInstance(mContext)
 				.getAccessToken(), Prefs.getInstance(mContext)
 				.getAccessTokenSecret());
@@ -105,6 +121,7 @@ public abstract class UserListFragment extends SherlockListFragment implements
 					.getInt(c
 							.getColumnIndex(ContactsContract.Columns.ContactGroups._ID));
 		}
+		c.close();
 	}
 
 	@Override
@@ -129,6 +146,26 @@ public abstract class UserListFragment extends SherlockListFragment implements
 		registerForContextMenu(getListView());
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.support.v4.app.ListFragment#onListItemClick(android.widget
+	 * .ListView , android.view.View, int, long)
+	 */
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		Cursor c = (Cursor) l.getItemAtPosition(position);
+
+		String userId = c.getString(c
+				.getColumnIndex(UsersContract.Columns.USER_ID));
+		if (userId != null) {
+			Intent intent = new Intent(mContext, UserDetailsActivity.class);
+			intent.putExtra(UsersContract.Columns.USER_ID, userId);
+			mContext.startActivity(intent);
+		}
+	}
+
 	/**
 	 * Creating floating context menu
 	 */
@@ -137,7 +174,44 @@ public abstract class UserListFragment extends SherlockListFragment implements
 			ContextMenuInfo menuInfo) {
 		super.onCreateContextMenu(menu, v, menuInfo);
 		MenuInflater inflater = getActivity().getMenuInflater();
-		inflater.inflate(R.menu.user_context_menu, menu);
+		AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+		Cursor listItemCursor = (Cursor) getListAdapter()
+				.getItem(info.position);
+
+		final String[] projection = new String[] {
+				ContactsContract.Qualified.ContactGroups.CONTACT_GROUPS_GROUP_NAME,
+				ContactsContract.Qualified.ContactGroups.CONTACT_GROUPS_GROUP_ID,
+				ContactsContract.Qualified.ContactGroups.CONTACT_GROUPS_ID };
+		final String contactId = listItemCursor.getString(listItemCursor
+				.getColumnIndex(UsersContract.Columns.USER_ID));
+
+		CursorLoader loader = new CursorLoader(getActivity(),
+				ContactsContract.CONTENT_URI_CONTACTS.buildUpon()
+						.appendPath(contactId).build(), projection, null, null,
+				ContactsContract.Columns.ContactGroups.GROUP_NAME + " ASC");
+
+		final Cursor c = loader.loadInBackground();
+
+		if (c.getCount() <= 0) {
+			inflater.inflate(R.menu.user_add_context_menu, menu);
+		} else {
+			inflater.inflate(R.menu.user_context_menu, menu);
+			c.moveToFirst();
+			while (!c.isAfterLast()) {
+				String currGroupName = c
+						.getString(c
+								.getColumnIndex(ContactsContract.Columns.ContactGroups.GROUP_NAME));
+				if (TextUtils.equals(currGroupName,
+						getString(R.string.studip_app_contacts_favorites))) {
+					menu.removeItem(R.id.add_to_favorites);
+					menu.findItem(R.id.remove_from_favorites).setVisible(true);
+				}
+
+				c.moveToNext();
+			}
+
+		}
+		c.close();
 	}
 
 	@Override
@@ -152,43 +226,45 @@ public abstract class UserListFragment extends SherlockListFragment implements
 			Cursor c = (Cursor) getListAdapter().getItem(info.position);
 			String userId = c.getString(c
 					.getColumnIndex(UsersContract.Columns.USER_ID));
-			String groupId = c
-					.getString(c
-							.getColumnIndex(ContactsContract.Columns.ContactGroups.GROUP_ID));
+
 			int userIntId = c.getInt(c
 					.getColumnIndex(UsersContract.Columns._ID));
 
 			switch (itemId) {
 			// add to or remove user from favorites
-			case R.id.add_remove_favorite:
-				if (TextUtils.equals(groupId, mFavoriteGroupId))
-					deleteUserFromGroup(userId, mFavoriteGroupId, userIntId);
-				else
-					addUserToGroup(userId, mFavoriteGroupId);
-
+			case R.id.add_to_favorites: {
+				addUserToGroup(userId, mFavoriteGroupId);
 				return true;
+			}
+			case R.id.remove_from_favorites: {
+				deleteUserFromGroup(userId, mFavoriteGroupId,
+						mFavoriteGroupIntId, userIntId);
+			}
 
-				// remove user from contacts
-			case R.id.remove_from_contacts:
+			// add to or remove from contacts
+			case R.id.add_to_contacts: {
+				addUserToContacts(userId);
+				return true;
+			}
+			case R.id.remove_from_contacts: {
 				deleteUserFromContacts(userId);
 				return true;
-
-				// add user to group
-			case R.id.add_to_group:
+			}
+			case R.id.manage_groups: {
 				Bundle args = new Bundle();
-				args.putString(UsersContract.Columns.USER_ID, userId);
-				ContactGroupsDialogFragment frag = new ContactGroupsDialogFragment();
+				args.putString(ContactsContract.Columns.Contacts.USER_ID,
+						userId);
+				args.putInt(ContactsContract.Columns.Contacts._ID, userIntId);
+				ContactGroupsDialogFragment frag = (ContactGroupsDialogFragment) ContactGroupsDialogFragment
+						.instantiate(mContext,
+								ContactGroupsDialogFragment.class.getName());
 				frag.setArguments(args);
 				getFragmentManager().beginTransaction()
 						.add(frag, ContactGroupsDialogFragment.class.getName())
 						.commit();
 				return true;
+			}
 
-				// remove user from selected group
-			case R.id.remove_from_group:
-				Log.i(TAG, "Add to group");
-				deleteUserFromGroup(userId, groupId, userIntId);
-				return true;
 			default:
 				return super.onContextItemSelected(item);
 			}
@@ -198,7 +274,50 @@ public abstract class UserListFragment extends SherlockListFragment implements
 
 	}
 
-	protected void deleteUserFromContacts(final String userId) {
+	private static void addUserToContacts(final String userId) {
+		JacksonRequest<Contacts> contactAddRequest = new JacksonRequest<Contacts>(
+				String.format(ApiEndpoints.CONTACTS_USER_ID_ENDPOINT, userId),
+				Contacts.class, null, new Listener<Contacts>() {
+					public void onResponse(Contacts response) {
+						try {
+							resolver.applyBatch(
+									AbstractContract.CONTENT_AUTHORITY,
+									new ContactsHandler(response).parse());
+						} catch (RemoteException e) {
+							e.printStackTrace();
+						} catch (OperationApplicationException e) {
+							e.printStackTrace();
+						}
+						Toast.makeText(mContext, "Erfolgreich hinzugefügt",
+								Toast.LENGTH_SHORT).show();
+					}
+				}, new ErrorListener() {
+					/*
+					 * (non-Javadoc)
+					 * 
+					 * @see com.android.volley.Response.ErrorListener
+					 * #onErrorResponse(com.android.volley. VolleyError)
+					 */
+					public void onErrorResponse(VolleyError error) {
+						Log.e(TAG, error.getMessage());
+						Toast.makeText(mContext,
+								"Fehler: " + error.getMessage(),
+								Toast.LENGTH_SHORT).show();
+					}
+				}, Method.PUT);
+		try {
+			mConsumer.sign(contactAddRequest);
+		} catch (OAuthMessageSignerException e) {
+			e.printStackTrace();
+		} catch (OAuthExpectationFailedException e) {
+			e.printStackTrace();
+		} catch (OAuthCommunicationException e) {
+			e.printStackTrace();
+		}
+		VolleyHttp.getRequestQueue().add(contactAddRequest);
+	}
+
+	private static void deleteUserFromContacts(final String userId) {
 		StringRequest request = new StringRequest(Method.DELETE, String.format(
 				ApiEndpoints.CONTACTS_USER_ID_ENDPOINT, userId),
 				new Listener<String>() {
@@ -211,12 +330,6 @@ public abstract class UserListFragment extends SherlockListFragment implements
 								Toast.LENGTH_SHORT).show();
 					}
 				}, new ErrorListener() {
-					/*
-					 * (non-Javadoc)
-					 * 
-					 * @see com.android.volley.Response.ErrorListener
-					 * #onErrorResponse(com.android.volley. VolleyError)
-					 */
 					public void onErrorResponse(VolleyError error) {
 						Log.e(TAG, error.getMessage());
 						Toast.makeText(mContext,
@@ -237,21 +350,19 @@ public abstract class UserListFragment extends SherlockListFragment implements
 		VolleyHttp.getRequestQueue().add(request);
 	}
 
-	protected void deleteUserFromGroup(final String userId,
-			final String groupId, final int IntUserId) {
+	private static void deleteUserFromGroup(final String userId,
+			final String groupId, final int groupIntId, final int userIntId) {
 		StringRequest request = new StringRequest(Method.DELETE, String.format(
 				ApiEndpoints.CONTACT_GROUPS_GROUP_ID_USER_ID_ENDPOINT, groupId,
 				userId), new Listener<String>() {
 			public void onResponse(String response) {
-				mContext.getContentResolver()
-						.delete(ContactsContract.CONTENT_URI_CONTACT_GROUP_MEMBERS
+				mContext.getContentResolver().delete(
+						ContactsContract.CONTENT_URI_CONTACT_GROUP_MEMBERS
 								.buildUpon()
-								.appendPath(
-										String.format("%d", mFavoriteGroupIntId))
+								.appendPath(String.format("%d", groupIntId))
 								.build(),
-								ContactsContract.Columns.Contacts.USER_ID
-										+ "= ?",
-								new String[] { String.format("%d", IntUserId) });
+						ContactsContract.Columns.Contacts.USER_ID + "= ?",
+						new String[] { String.format("%d", userIntId) });
 				Toast.makeText(mContext, "Erfolgreich gelöscht",
 						Toast.LENGTH_SHORT).show();
 			}
@@ -281,7 +392,7 @@ public abstract class UserListFragment extends SherlockListFragment implements
 		VolleyHttp.getRequestQueue().add(request);
 	}
 
-	protected void addUserToGroup(final String userId, final String groupId) {
+	private static void addUserToGroup(final String userId, final String groupId) {
 		JacksonRequest<ContactGroups> userAddRequest = new JacksonRequest<ContactGroups>(
 				String.format(
 						ApiEndpoints.CONTACT_GROUPS_GROUP_ID_USER_ID_ENDPOINT,
@@ -289,7 +400,7 @@ public abstract class UserListFragment extends SherlockListFragment implements
 				new Listener<ContactGroups>() {
 					public void onResponse(ContactGroups response) {
 						try {
-							mContext.getContentResolver().applyBatch(
+							resolver.applyBatch(
 									AbstractContract.CONTENT_AUTHORITY,
 									new ContactGroupHandler(response.group)
 											.parse());
@@ -341,10 +452,10 @@ public abstract class UserListFragment extends SherlockListFragment implements
 				ContactsContract.Qualified.ContactGroups.CONTACT_GROUPS_GROUP_ID };
 	}
 
-	@SuppressLint("ValidFragment")
-	public class ContactGroupsDialogFragment extends DialogFragment {
+	public static class ContactGroupsDialogFragment extends DialogFragment {
 
-		private Bundle mArgs;
+		private String mUserId;
+		private int mIntUserId;
 
 		/*
 		 * (non-Javadoc)
@@ -355,32 +466,123 @@ public abstract class UserListFragment extends SherlockListFragment implements
 		@Override
 		public void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
-			mArgs = getArguments();
+			mUserId = getArguments().getString(
+					ContactsContract.Columns.Contacts.USER_ID);
+			mIntUserId = getArguments().getInt(
+					ContactsContract.Columns.Contacts._ID);
 		}
 
 		@Override
 		public Dialog onCreateDialog(Bundle savedInstanceState) {
-			String[] projection = new String[] {
-					ContactsContract.Columns.ContactGroups.GROUP_NAME,
-					ContactsContract.Columns.ContactGroups.GROUP_ID,
-					ContactsContract.Columns.ContactGroups._ID };
+			final String[] projection = new String[] {
+					ContactsContract.Qualified.ContactGroups.CONTACT_GROUPS_GROUP_NAME,
+					ContactsContract.Qualified.ContactGroups.CONTACT_GROUPS_GROUP_ID,
+					ContactsContract.Qualified.ContactGroups.CONTACT_GROUPS_ID };
 
-			CursorLoader loader = new CursorLoader(getActivity(),
+			final HashMap<String, Pair<Pair<String, Integer>, Boolean>> multimap = new HashMap<String, Pair<Pair<String, Integer>, Boolean>>();
+			final ArrayList<Pair<String, Integer>> allGroupIds = new ArrayList<Pair<String, Integer>>();
+			final ArrayList<String> allGroupNames = new ArrayList<String>();
+
+			// load all groups
+			CursorLoader allGroupsloader = new CursorLoader(getActivity(),
 					ContactsContract.CONTENT_URI_CONTACT_GROUPS, projection,
 					null, null,
 					ContactsContract.Columns.ContactGroups.GROUP_NAME + " ASC");
-			final Cursor c = loader.loadInBackground();
-			AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-			builder.setTitle(R.string.choose_group);
-			builder.setCursor(c, new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int which) {
-					c.moveToPosition(which);
-					addUserToGroup(
-							mArgs.getString(UsersContract.Columns.USER_ID),
-							c.getString(c
-									.getColumnIndex(ContactsContract.Columns.ContactGroups.GROUP_ID)));
+			final Cursor cursorAllGroups = allGroupsloader.loadInBackground();
+			cursorAllGroups.moveToFirst();
+			while (!cursorAllGroups.isAfterLast()) {
+				String groupName = cursorAllGroups
+						.getString(cursorAllGroups
+								.getColumnIndex(ContactsContract.Columns.ContactGroups.GROUP_NAME));
+				String groupId = cursorAllGroups
+						.getString(cursorAllGroups
+								.getColumnIndex(ContactsContract.Columns.ContactGroups.GROUP_ID));
+				int groupIntId = cursorAllGroups
+						.getInt(cursorAllGroups
+								.getColumnIndex(ContactsContract.Columns.ContactGroups._ID));
+				multimap.put(groupName,
+						new Pair<Pair<String, Integer>, Boolean>(
+								new Pair<String, Integer>(groupId, groupIntId),
+								false));
+				cursorAllGroups.moveToNext();
+			}
+			cursorAllGroups.close();
+
+			// load only the users groups
+			CursorLoader selectedGroupsLoader = new CursorLoader(getActivity(),
+					ContactsContract.CONTENT_URI_CONTACTS.buildUpon()
+							.appendPath(mUserId).build(), projection, null,
+					null, ContactsContract.Columns.ContactGroups.GROUP_NAME
+							+ " ASC");
+			final Cursor userGroupsCursor = selectedGroupsLoader
+					.loadInBackground();
+			userGroupsCursor.moveToFirst();
+
+			// set selected if group is users group
+			while (!userGroupsCursor.isAfterLast()) {
+				String userGroup = userGroupsCursor
+						.getString(cursorAllGroups
+								.getColumnIndex(ContactsContract.Columns.ContactGroups.GROUP_NAME));
+				if (multimap.containsKey(userGroup)) {
+					Pair<String, Integer> groupIdPair = multimap.get(userGroup).first;
+					multimap.put(userGroup,
+							new Pair<Pair<String, Integer>, Boolean>(
+									groupIdPair, true));
 				}
-			}, ContactsContract.Columns.ContactGroups.GROUP_NAME);
+				userGroupsCursor.moveToNext();
+			}
+			userGroupsCursor.close();
+
+			Iterator<Entry<String, Pair<Pair<String, Integer>, Boolean>>> it = multimap
+					.entrySet().iterator();
+			int row = 0;
+			boolean[] primitivValuesArr = new boolean[multimap.size()];
+			while (it.hasNext()) {
+				Map.Entry<String, Pair<Pair<String, Integer>, Boolean>> pairs = (Map.Entry<String, Pair<Pair<String, Integer>, Boolean>>) it
+						.next();
+
+				allGroupNames.add(pairs.getKey());
+				allGroupIds.add(pairs.getValue().first);
+				primitivValuesArr[row] = pairs.getValue().second;
+
+				row++;
+				it.remove(); // avoids a ConcurrentModificationException
+			}
+
+			AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+			builder.setTitle(R.string.manage_groups);
+			builder.setMultiChoiceItems(allGroupNames
+					.toArray(new CharSequence[allGroupNames.size()]),
+					primitivValuesArr, new OnMultiChoiceClickListener() {
+
+						public void onClick(DialogInterface dialog, int which,
+								boolean isChecked) {
+							if (isChecked) {
+								addUserToGroup(mUserId,
+										allGroupIds.get(which).first);
+							} else {
+								deleteUserFromGroup(mUserId,
+										allGroupIds.get(which).first,
+										allGroupIds.get(which).second,
+										mIntUserId);
+							}
+
+						}
+					});
+			builder.setPositiveButton(android.R.string.ok,
+					new DialogInterface.OnClickListener() {
+						/*
+						 * (non-Javadoc)
+						 * 
+						 * @see
+						 * android.content.DialogInterface.OnClickListener#onClick
+						 * (android.content.DialogInterface, int)
+						 */
+						public void onClick(DialogInterface dialog, int which) {
+							getDialog().cancel();
+						}
+					});
+
 			return builder.create();
 		}
 	}
