1eacb72c7fa24692e806f3a11c76f5a5a1437cb1
Jörn <j.domnik@googlemail.com>
2013-10-05 13:10:25 +0200

    Added request debug code

--- StudIPAndroidApp/src/de/elanev/studip/android/app/backend/net/util/JacksonRequest.java
+++ StudIPAndroidApp/src/de/elanev/studip/android/app/backend/net/util/JacksonRequest.java
@@ -7,9 +7,7 @@
  ******************************************************************************/
 package de.elanev.studip.android.app.backend.net.util;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.Map;
+import android.util.Log;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.NetworkResponse;
@@ -23,92 +21,127 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+
 import de.elanev.studip.android.app.backend.datamodel.Message;
 import de.elanev.studip.android.app.backend.datamodel.User;
 import de.elanev.studip.android.app.backend.net.oauth.VolleySignPostRequestWrapper;
 
 /**
  * @author joern
- * 
  */
 public class JacksonRequest<T> extends VolleySignPostRequestWrapper<T> {
+    public static final String TAG = JacksonRequest.class.getSimpleName();
+    private final Class<T> clazz;
+    private final Map<String, String> headers;
+    private final Listener<T> listener;
+    private boolean isDebugable = false;
 
-	private final Class<T> clazz;
-	private final Map<String, String> headers;
-	private final Listener<T> listener;
+    /**
+     * Make a GET request and return a parsed object from JSON.
+     *
+     * @param url     URL of the request to make
+     * @param clazz   Relevant class object, for Gson's reflection
+     * @param headers Map of request headers
+     * @param method  HTTP Method to be used. See
+     *                {@link com.android.volley.Request.Method<T>}
+     */
+    public JacksonRequest(String url, Class<T> clazz,
+                          Map<String, String> headers, Listener<T> listener,
+                          ErrorListener errorListener, int method) {
+        super(method, url, errorListener);
+        this.clazz = clazz;
+        this.headers = headers;
+        this.listener = listener;
 
-	/**
-	 * Make a GET request and return a parsed object from JSON.
-	 * 
-	 * @param url
-	 *            URL of the request to make
-	 * @param clazz
-	 *            Relevant class object, for Gson's reflection
-	 * @param headers
-	 *            Map of request headers
-	 * @param method
-	 *            HTTP Method to be used. See
-	 *            {@link com.android.volley.Request.Method<T>}
-	 */
-	public JacksonRequest(String url, Class<T> clazz,
-			Map<String, String> headers, Listener<T> listener,
-			ErrorListener errorListener, int method) {
-		super(method, url, errorListener);
-		this.clazz = clazz;
-		this.headers = headers;
-		this.listener = listener;
+    }
 
-	}
+    /**
+     * Make a GET request and return a parsed object from JSON.
+     *
+     * @param url     URL of the request to make
+     * @param clazz   Relevant class object, for Gson's reflection
+     * @param headers Map of request headers
+     * @param method  HTTP Method to be used. See
+     *                {@link com.android.volley.Request.Method<T>}
+     */
+    public JacksonRequest(String url, Class<T> clazz,
+                          Map<String, String> headers, Listener<T> listener,
+                          ErrorListener errorListener, int method, boolean isDebugable) {
+        super(method, url, errorListener);
+        this.clazz = clazz;
+        this.headers = headers;
+        this.listener = listener;
+        this.isDebugable = isDebugable;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * de.elanev.studip.android.app.backend.net.oauth.VolleySignPostRequestWrapper
-	 * #getHeaders()
-	 */
-	@Override
-	public Map<String, String> getHeaders() throws AuthFailureError {
-		return headers != null ? headers : super.getHeaders();
-	}
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.android.volley.Request#parseNetworkResponse(com.android.volley.
-	 * NetworkResponse)
-	 */
-	@Override
-	protected Response<T> parseNetworkResponse(NetworkResponse response) {
-		try {
-			ObjectMapper mapper = new ObjectMapper();
-			// unwrap root elements in specific jsons
-			if (clazz.equals(User.class) || clazz.equals(Message.class))
-				mapper.configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true);
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * de.elanev.studip.android.app.backend.net.oauth.VolleySignPostRequestWrapper
+     * #getHeaders()
+     */
+    @Override
+    public Map<String, String> getHeaders() throws AuthFailureError {
+        return headers != null ? headers : super.getHeaders();
+    }
 
-			// return the parsed response
-			return Response.success(mapper.readValue(new String(response.data,
-					HttpHeaderParser.parseCharset(response.headers)), clazz),
-					HttpHeaderParser.parseCacheHeaders(response));
-		} catch (UnsupportedEncodingException e) {
-			return Response.error(new ParseError(e));
-		} catch (JsonParseException e) {
-			return Response.error(new ParseError(e));
-		} catch (JsonMappingException e) {
-			return Response.error(new ParseError(e));
-		} catch (IOException e) {
-			return Response.error(new ParseError(e));
-		}
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see com.android.volley.Request#parseNetworkResponse(com.android.volley.
+     * NetworkResponse)
+     */
+    @Override
+    protected Response<T> parseNetworkResponse(NetworkResponse response) {
+        String str = "";
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            // unwrap root elements in specific jsons
+            if (clazz.equals(User.class) || clazz.equals(Message.class))
+                mapper.configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true);
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.android.volley.Request#deliverResponse(java.lang.Object)
-	 */
-	@Override
-	protected void deliverResponse(T response) {
-		listener.onResponse(response);
-	}
+            // return the parsed response
+            if (isDebugable)
+                str = new String(response.data,
+                        HttpHeaderParser.parseCharset(response.headers));
+
+            return Response.success(
+                    mapper.readValue(new String(response.data,
+                            HttpHeaderParser.parseCharset(response.headers)), clazz),
+                    HttpHeaderParser.parseCacheHeaders(response));
+        } catch (UnsupportedEncodingException e) {
+            logResponse("Request: " + getMethod() + " " + getUrl() + "\nResponse: " + str + "\nError: " + e.getMessage());
+            return Response.error(new ParseError(e));
+        } catch (JsonParseException e) {
+            logResponse("Request: " + getMethod() + " " + getUrl() + "\nResponse: " + str + "\nError: " + e.getMessage());
+            return Response.error(new ParseError(e));
+        } catch (JsonMappingException e) {
+            logResponse("Request: " + getMethod() + " " + getUrl() + "\nResponse: " + str + "\nError: " + e.getMessage());
+            return Response.error(new ParseError(e));
+        } catch (IOException e) {
+            logResponse("Request: " + getMethod() + " " + getUrl() + "\nResponse: " + str + "\nError: " + e.getMessage());
+            return Response.error(new ParseError(e));
+        }
+    }
+
+    private void logResponse(String str) {
+        if (isDebugable)
+            Log.wtf(TAG, str);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see com.android.volley.Request#deliverResponse(java.lang.Object)
+     */
+    @Override
+    protected void deliverResponse(T response) {
+        listener.onResponse(response);
+    }
 
 }
