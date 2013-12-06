5792fc988248fbdd50a242a40769d36d6b91caaf
Jörn <j.domnik@googlemail.com>
2013-12-06 01:54:19 +0100

    Removed unused code due to change of OAuth workflow in SignInActivity

--- StudIPAndroidApp/src/de/elanev/studip/android/app/backend/net/oauth/WebViewActivity.java
+++ StudIPAndroidApp/src/de/elanev/studip/android/app/backend/net/oauth/WebViewActivity.java
@@ -13,7 +13,6 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.util.Log;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -21,107 +20,83 @@ import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.MenuItem;
 
 import de.elanev.studip.android.app.R;
-import oauth.signpost.OAuthConsumer;
 
 /**
  * Activity holding a WebView which prompts the user the OAuth permission
- * 
+ *
  * @author joern
  */
 public class WebViewActivity extends SherlockActivity {
 
-	public static final String TAG = WebViewActivity.class.getSimpleName();
-	private WebView mWebview;
+    public static final String TAG = WebViewActivity.class.getSimpleName();
+    private WebView mWebview;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see android.app.Activity#onCreate(android.os.Bundle)
-	 */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-		setTitle(android.R.string.cancel);
-		setContentView(R.layout.webview_view);
+        setTitle(android.R.string.cancel);
+        setContentView(R.layout.webview_view);
 
-		mWebview = (WebView) this.findViewById(R.id.webView);
+        mWebview = (WebView) this.findViewById(R.id.webView);
 
-		// If instance state was saved, restore it
-		if (savedInstanceState != null) {
-			mWebview.restoreState(savedInstanceState);
-		} else {
-			mWebview.setWebViewClient(new LoginWebViewClient());
-			Intent intent = getIntent();
-			Bundle extras = intent.getExtras();
-			String authUrl = extras.getString("sAuthUrl");
-			mWebview.loadUrl(authUrl);
-		}
+        // If instance state was saved, restore it
+        if (savedInstanceState != null) {
+            mWebview.restoreState(savedInstanceState);
+        } else {
+            mWebview.setWebViewClient(new LoginWebViewClient());
+            Intent intent = getIntent();
+            Bundle extras = intent.getExtras();
+            String authUrl = extras.getString("sAuthUrl");
+            mWebview.loadUrl(authUrl);
+        }
 
-	}
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.actionbarsherlock.app.SherlockActivity#onSaveInstanceState(android
-	 * .os.Bundle)
-	 */
-	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-		mWebview.saveState(outState);
-	}
+    /*
+    * Prevent the user from pressing the back button
+    */
+    @Override
+    public void onBackPressed() {
+        return;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.actionbarsherlock.app.SherlockActivity#onOptionsItemSelected(android
-	 * .view.MenuItem)
-	 */
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		// Respond to the action bar's Up/Home button
-		case android.R.id.home:
-			NavUtils.navigateUpTo(this, new Intent(this, SignInActivity.class));
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        mWebview.saveState(outState);
+    }
 
-	/*
-	 * WebviewClient which overrides the onPageStarted method to intercept the
-	 * OAuth result
-	 */
-	private class LoginWebViewClient extends WebViewClient {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpTo(this, new Intent(this, SignInActivity.class));
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-		public final String TAG = LoginWebViewClient.class.getCanonicalName();
+    /*
+     * WebviewClient which overrides the onPageStarted method to intercept the
+     * OAuth result
+     */
+    private class LoginWebViewClient extends WebViewClient {
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see
-		 * android.webkit.WebViewClient#onPageStarted(android.webkit.WebView,
-		 * java.lang.String, android.graphics.Bitmap)
-		 */
-		@Override
-		public void onPageStarted(WebView view, String url, Bitmap favicon) {
-			if (url.contains("user")) {
-				Log.i(TAG, "AUTHURL" + url);
+        public final String TAG = LoginWebViewClient.class.getCanonicalName();
 
-				Intent intent = new Intent();
-				OAuthConsumer consumer = OAuthConnector.getConsumer();
-				intent.putExtra("token", consumer.getToken());
-				intent.putExtra("tokenSecret", consumer.getTokenSecret());
-				Log.i(TAG,
-						consumer.getToken() + " " + consumer.getTokenSecret());
-				setResult(RESULT_OK, intent);
-				finish();
-			}
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            if (url.contains("user")) {
+                Log.i(TAG, "AUTHURL" + url);
 
-		}
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
 
-	}
+        }
+
+    }
 }
