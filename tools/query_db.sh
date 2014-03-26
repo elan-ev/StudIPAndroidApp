43e9bb4b2c3e8b7c78e6cf3563a7436d3d4c8843
Jörn <j.domnik@googlemail.com>
2014-03-26 22:35:46 +0100

    query new debug database

--- tools/query_db.sh
+++ tools/query_db.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [[ -z $ADB ]]; then ADB=adb; fi
-$ADB shell su -c cp /data/data/de.elanev.studip.android.app/databases/studip.db	/storage/sdcard0
-$ADB pull /storage/sdcard0/studip.db
-$ADB shell su -c rm /storage/sdcard0/studip.db	
-echo "$@;" | sqlite3 -header -column studip.db
+$ADB shell su -c cp /data/data/de.elanev.studip.android.app/databases/studip_debug.db	/storage/sdcard0
+$ADB pull /storage/sdcard0/studip_debug.db
+$ADB shell su -c rm /storage/sdcard0/studip_debug.db	
+echo "$@;" | sqlite3 -header -column studip_debug.db
