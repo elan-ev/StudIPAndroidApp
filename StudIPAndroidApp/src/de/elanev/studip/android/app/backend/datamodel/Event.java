6c9f0c62e049fe54203c8dc3bf36c5948bf1ba64
Jörn <j.domnik@googlemail.com>
2014-04-04 21:14:38 +0200

    Added course title property for planner

--- StudIPAndroidApp/src/de/elanev/studip/android/app/backend/datamodel/Event.java
+++ StudIPAndroidApp/src/de/elanev/studip/android/app/backend/datamodel/Event.java
@@ -6,7 +6,7 @@
  * http://www.gnu.org/licenses/gpl.html
  ******************************************************************************/
 /**
- * 
+ *
  */
 package de.elanev.studip.android.app.backend.datamodel;
 
@@ -14,42 +14,43 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 /**
  * @author joern
- * 
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Event {
-	public String event_id;
-	public String course_id;
-	public Long start;
-	public Long end;
-	public String title;
-	public String description;
-	public String categories;
-	public String room;
+    public String event_id;
+    public String course_id;
+    public Long start;
+    public Long end;
+    public String title;
+    public String description;
+    public String categories;
+    public String room;
+    // Special property for the planner
+    public String courseTitle;
 
-	public Event() {
-	}
+    public Event() {
+    }
 
-	/**
-	 * @param event_id
-	 * @param course_id
-	 * @param start
-	 * @param end
-	 * @param title
-	 * @param description
-	 * @param categories
-	 * @param room
-	 */
-	public Event(String event_id, String course_id, Long start, Long end,
-			String title, String description, String categories, String room) {
-		this.event_id = event_id;
-		this.course_id = course_id;
-		this.start = start;
-		this.end = end;
-		this.title = title;
-		this.description = description;
-		this.categories = categories;
-		this.room = room;
-	}
+    /**
+     * @param event_id
+     * @param course_id
+     * @param start
+     * @param end
+     * @param title
+     * @param description
+     * @param categories
+     * @param room
+     */
+    public Event(String event_id, String course_id, Long start, Long end,
+                 String title, String description, String categories, String room) {
+        this.event_id = event_id;
+        this.course_id = course_id;
+        this.start = start;
+        this.end = end;
+        this.title = title;
+        this.description = description;
+        this.categories = categories;
+        this.room = room;
+    }
 
 }
