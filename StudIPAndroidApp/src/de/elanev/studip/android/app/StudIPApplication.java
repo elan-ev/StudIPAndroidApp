a25c99bfe6541b1671ef872d8ba76e0ceee7144d
Jörn <j.domnik@googlemail.com>
2014-04-23 15:41:36 +0200

    Temp disabled Debug Strict mode duo to okttp bug
    Fix was merged https://github.com/square/okhttp/pull/737, now we wait
    until its being pushed to maven central

--- StudIPAndroidApp/src/de/elanev/studip/android/app/StudIPApplication.java
+++ StudIPAndroidApp/src/de/elanev/studip/android/app/StudIPApplication.java
@@ -3,7 +3,6 @@ package de.elanev.studip.android.app;
 import android.annotation.TargetApi;
 import android.app.Application;
 import android.os.Build;
-import android.os.StrictMode;
 import android.text.TextUtils;
 
 import com.android.volley.Request;
@@ -33,97 +32,88 @@ import de.elanev.studip.android.app.backend.net.util.OkHttpStack;
  */
 public class StudIPApplication extends Application {
 
-    public static String TAG = StudIPApplication.class.getSimpleName();
-    private static StudIPApplication mInstance;
-    private RequestQueue mRequestQueue;
+  public static String TAG = StudIPApplication.class.getSimpleName();
+  private static StudIPApplication mInstance;
+  private RequestQueue mRequestQueue;
 
-    public static synchronized StudIPApplication getInstance() {
-        return mInstance;
-    }
+  public static synchronized StudIPApplication getInstance() {
+    return mInstance;
+  }
 
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    @Override
-    public void onCreate() {
-        super.onCreate();
+  @TargetApi(Build.VERSION_CODES.HONEYCOMB) @Override
+  public void onCreate() {
+    super.onCreate();
 
-        // create instance
-        mInstance = this;
+    // create instance
+    mInstance = this;
 
-        // Trigger initialization of Crashlytics
-        if (BuildConfig.USE_CRASHLYTICS)
-            Crashlytics.start(this);
+    // Trigger initialization of Crashlytics
+    if (BuildConfig.USE_CRASHLYTICS) Crashlytics.start(this);
 
-        // Load SQLCipher JNI Libs
-        SQLiteDatabase.loadLibs(this);
+    // Load SQLCipher JNI Libs
+    SQLiteDatabase.loadLibs(this);
+    //TODO: Update OKHTTP Lib after the ResponceCache fix is in v > 1.5.4
+    //        if(BuildConfig.DEBUG) {
+    //                StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+    //                        .detectDiskReads()
+    //                        .detectDiskWrites()
+    //                        .detectNetwork()   // or .detectAll() for all detectable problems
+    //                        .penaltyLog()
+    //                        .build());
+    //                StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+    //                        .detectLeakedSqlLiteObjects()
+    //                        .detectLeakedClosableObjects()
+    //                        .penaltyLog()
+    //                        .penaltyDeath()
+    //                        .build());
+    //
+    //        }
+  }
 
-        /*
-         * Clear shared prefs for debugging
-		 */
-        //Prefs.getInstance(getApplicationContext()).clearPrefs();
+  /**
+   * Adds a Volley Request<T> to the RequestQueue of the Application Context using the default
+   * tag
+   *
+   * @param request the Volley Request to add to the queue
+   */
+  public <T> void addToRequestQueue(Request<T> request) {
+    addToRequestQueue(request, "");
+  }
 
-        if(BuildConfig.DEBUG) {
-                StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
-                        .detectDiskReads()
-                        .detectDiskWrites()
-                        .detectNetwork()   // or .detectAll() for all detectable problems
-                        .penaltyLog()
-                        .build());
-                StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
-                        .detectLeakedSqlLiteObjects()
-                        .detectLeakedClosableObjects()
-                        .penaltyLog()
-                        .penaltyDeath()
-                        .build());
+  /**
+   * Adds a Volley Request<T> to the RequestQueue of the Application Context using the supplied
+   * tag.
+   *
+   * @param request the Volley Request to add to the queue
+   * @param tag     the tag for this request, empty string will force default tag
+   */
+  public <T> void addToRequestQueue(Request<T> request, String tag) {
+    request.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
 
-        }
-    }
+    getRequestQueue().add(request);
+  }
 
-    /**
-     * Returns a RequestQueue using the ApplicationContext.
-     *
-     * @return RequestQueue
-     */
-    public RequestQueue getRequestQueue() {
-        // Create new if no queue is null
-        if (mRequestQueue == null)
-            mRequestQueue = Volley.newRequestQueue(getApplicationContext(),
-                    new OkHttpStack());
+  /**
+   * Returns a RequestQueue using the ApplicationContext.
+   *
+   * @return RequestQueue
+   */
+  public RequestQueue getRequestQueue() {
+    // Create new if no queue is null
+    if (mRequestQueue == null)
+      mRequestQueue = Volley.newRequestQueue(getApplicationContext(), new OkHttpStack());
 
-        return mRequestQueue;
-    }
+    return mRequestQueue;
+  }
 
-    /**
-     * Adds a Volley Request<T> to the RequestQueue of the Application Context using the supplied
-     * tag.
-     *
-     * @param request the Volley Request to add to the queue
-     * @param tag     the tag for this request, empty string will force default tag
-     */
-    public <T> void addToRequestQueue(Request<T> request, String tag) {
-        request.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
-
-        getRequestQueue().add(request);
-    }
-
-    /**
-     * Adds a Volley Request<T> to the RequestQueue of the Application Context using the default
-     * tag
-     *
-     * @param request the Volley Request to add to the queue
-     */
-    public <T> void addToRequestQueue(Request<T> request) {
-        addToRequestQueue(request, "");
-    }
-
-    /**
-     * Cancels all pending Volley Requests waiting in the queue and are tagged with the supplied
-     * tag Object.
-     *
-     * @param tag the tag Object to identify the requests to cancel
-     */
-    public void cancelAllPendingRequests(Object tag) {
-        if (mRequestQueue != null)
-            mRequestQueue.cancelAll(tag == null ? TAG : tag);
-    }
+  /**
+   * Cancels all pending Volley Requests waiting in the queue and are tagged with the supplied
+   * tag Object.
+   *
+   * @param tag the tag Object to identify the requests to cancel
+   */
+  public void cancelAllPendingRequests(Object tag) {
+    if (mRequestQueue != null) mRequestQueue.cancelAll(tag == null ? TAG : tag);
+  }
 
 }
