29c731cd2563338da4b30fcae498e508d5789287
Jörn <j.domnik@googlemail.com>
2014-04-23 11:24:54 +0200

    Fixed crash with empty title resource id

--- StudIPAndroidApp/src/de/elanev/studip/android/app/widget/WebViewActivity.java
+++ StudIPAndroidApp/src/de/elanev/studip/android/app/widget/WebViewActivity.java
@@ -24,42 +24,42 @@ import de.elanev.studip.android.app.R;
  */
 public class WebViewActivity extends SherlockActivity {
 
-    public static final String TAG = WebViewActivity.class.getSimpleName();
-    public static final String URL = "url";
-    public static final String TITLE_RES = "title_res";
-    private WebView mWebview;
-    private String mUrl;
-    private int mTitleRes;
+  public static final String TAG = WebViewActivity.class.getSimpleName();
+  public static final String URL = "url";
+  public static final String TITLE_RES = "title_res";
+  private WebView mWebview;
+  private String mUrl;
+  private int mTitleRes;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // Set up layout
-        setContentView(R.layout.webview_view);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        setTitle(mTitleRes);
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    // Set up layout
+    setContentView(R.layout.webview_view);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        // Get Intent data
-        Intent intent = getIntent();
-        Bundle extras = intent.getExtras();
-        mUrl = extras.getString(URL);
-        mTitleRes = extras.getInt(TITLE_RES);
+    // Get Intent data
+    Intent intent = getIntent();
+    Bundle extras = intent.getExtras();
+    mUrl = extras.getString(URL);
+    mTitleRes = extras.getInt(TITLE_RES);
+    setTitle(mTitleRes);
 
-        // Do the loading
-        mWebview = (WebView) this.findViewById(R.id.webView);
-        mWebview.loadUrl(mUrl);
+    // Do the loading
+    mWebview = (WebView) this.findViewById(R.id.webView);
+    mWebview.loadUrl(mUrl);
 
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      // Respond to the action bar's Up/Home button
+      case android.R.id.home:
+        onBackPressed();
+        return true;
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            // Respond to the action bar's Up/Home button
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+    return super.onOptionsItemSelected(item);
+  }
 
 }
