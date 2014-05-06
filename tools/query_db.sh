859a6bde9cd465f4b6cafa9640cc4c8ab130c288
Jörn <j.domnik@googlemail.com>
2014-05-06 23:36:31 +0200

    Now works with Genymotion VMs too.

--- tools/query_db.sh
+++ tools/query_db.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 if [[ -z $ADB ]]; then ADB=adb; fi
-$ADB shell su -c cp /data/data/de.elanev.studip.android.app/databases/studip_debug.db	/storage/sdcard0
-$ADB pull /storage/sdcard0/studip_debug.db
-$ADB shell su -c rm /storage/sdcard0/studip_debug.db	
+  if [[ $EUID -ne 0 ]]; then
+    echo "WE ARE ROOT! Running directly."
+    $ADB shell cp /data/data/de.elanev.studip.android.app/databases/studip_debug.db	/storage/sdcard0
+    $ADB pull /storage/sdcard0/studip_debug.db
+    $ADB shell rm /storage/sdcard0/studip_debug.db
+  else
+    echo "NO ROOT :(, trying su.."
+    $ADB shell su -c cp /data/data/de.elanev.studip.android.app/databases/studip_debug.db	/storage/sdcard0
+    $ADB pull /storage/sdcard0/studip_debug.db
+    $ADB shell su -c rm /storage/sdcard0/studip_debug.db
+  fi
 echo "$@;" | sqlite3 -header -column studip_debug.db
