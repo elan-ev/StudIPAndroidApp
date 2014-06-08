bd51d8dffb154b081c4d82591e94e98d3bf8ca8d
aklassen <aklassen@uos.de>
2014-06-08 18:06:58 +0200

    refine ProfileFragmen. Prototypical use of UserDetailsActivity refs #16

--- StudIPAndroidApp/src/main/java/de/elanev/studip/android/app/frontend/profile/ProfileFragment.java
+++ StudIPAndroidApp/src/main/java/de/elanev/studip/android/app/frontend/profile/ProfileFragment.java
@@ -1,47 +1,11 @@
 package de.elanev.studip.android.app.frontend.profile;
 
-import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.android.volley.DefaultRetryPolicy;
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Locale;
-
-import de.elanev.studip.android.app.R;
-import de.elanev.studip.android.app.StudIPApplication;
-import de.elanev.studip.android.app.backend.datamodel.Event;
-import de.elanev.studip.android.app.backend.datamodel.Events;
-import de.elanev.studip.android.app.backend.datamodel.Server;
-import de.elanev.studip.android.app.backend.db.CoursesContract;
-import de.elanev.studip.android.app.backend.net.oauth.OAuthConnector;
-import de.elanev.studip.android.app.backend.net.util.JacksonRequest;
-import de.elanev.studip.android.app.frontend.courses.CourseViewActivity;
+import de.elanev.studip.android.app.backend.db.UsersContract;
 import de.elanev.studip.android.app.util.Prefs;
-import de.elanev.studip.android.app.util.StuffUtil;
-import de.elanev.studip.android.app.util.TextTools;
 import de.elanev.studip.android.app.widget.ProgressSherlockListFragment;
-import oauth.signpost.exception.OAuthCommunicationException;
-import oauth.signpost.exception.OAuthExpectationFailedException;
-import oauth.signpost.exception.OAuthMessageSignerException;
-import oauth.signpost.exception.OAuthNotAuthorizedException;
-import se.emilsjolander.stickylistheaders.StickyListHeadersAdapter;
+import de.elanev.studip.android.app.widget.UserDetailsActivity;
 
 
 
@@ -59,9 +23,14 @@ public class ProfileFragment extends ProgressSherlockListFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //mContext = getSherlockActivity();
-        //mServer = Prefs.getInstance(mContext).getServer();
-        //mEventsRoute = String.format(getString(R.string.restip_planner) + ".json", mServer.getApiUrl());
+        mContext = getSherlockActivity();
+        String userId = Prefs.getInstance(mContext).getUserId();
+
+        if (userId != null) {
+            Intent intent = new Intent(mContext, UserDetailsActivity.class);
+            intent.putExtra(UsersContract.Columns.USER_ID, userId);
+            mContext.startActivity(intent);
+        }
 
     }
 }
