f76d6d351dcaa009b0e79207c477136f45bfb776
Jörn D. <j.domnik@googlemail.com>
2014-07-09 18:53:10 +0200

    Formatting and copyright update

--- StudIPAndroidApp/src/main/java/de/elanev/studip/android/app/frontend/courses/CourseViewActivity.java
+++ StudIPAndroidApp/src/main/java/de/elanev/studip/android/app/frontend/courses/CourseViewActivity.java
@@ -1,10 +1,10 @@
-/*******************************************************************************
- * Copyright (c) 2013 ELAN e.V.
+/*
+ * Copyright (c) 2014 ELAN e.V.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
- ******************************************************************************/
+ */
 package de.elanev.studip.android.app.frontend.courses;
 
 import android.os.Bundle;
@@ -26,96 +26,92 @@ import de.elanev.studip.android.app.frontend.util.TabbedFragmentActivity;
  * schedule, participants and documents.
  */
 public class CourseViewActivity extends TabbedFragmentActivity {
-    ViewPager mPager;
-    BasePagerTabsAdapter mPagerAdapter;
+  ViewPager mPager;
+  BasePagerTabsAdapter mPagerAdapter;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
 
-        getSupportActionBar().setHomeButtonEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    getSupportActionBar().setHomeButtonEnabled(true);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        // Get intent data
-        Bundle intentExtras = getIntent().getExtras();
+    // Get intent data
+    Bundle intentExtras = getIntent().getExtras();
 
-        // ViewPager, PagetAdapter setup
-        mPager = new ViewPager(this);
-        mPager.setId("VP".hashCode());
-        setContentView(mPager);
-        mPagerAdapter = new BasePagerTabsAdapter(this,
-                getSupportFragmentManager(),
-                mPager,
-                mActionbar);
+    // ViewPager, PagetAdapter setup
+    mPager = new ViewPager(this);
+    mPager.setId("VP".hashCode());
+    setContentView(mPager);
+    mPagerAdapter = new BasePagerTabsAdapter(this, getSupportFragmentManager(), mPager, mActionbar);
 
-        // Add the tabs to the PagerAdapter
-        mPagerAdapter.addTab(mActionbar.newTab(),
-                R.drawable.ic_action_seminar,
-                R.string.Overview,
-                CourseOverviewFragment.class,
-                intentExtras);
-        mPagerAdapter.addTab(mActionbar.newTab(),
-                R.drawable.ic_action_schedule,
-                R.string.Schedule,
-                CourseScheduleFragment.class,
-                intentExtras);
-        mPagerAdapter.addTab(mActionbar.newTab(),
-                R.drawable.ic_action_attendees,
-                R.string.attendees,
-                CourseAttendeesFragment.class,
-                intentExtras);
-        mPagerAdapter.addTab(mActionbar.newTab(),
-                R.drawable.ic_action_files,
-                R.string.Documents,
-                CourseDocumentsFragment.class,
-                intentExtras);
+    // Add the tabs to the PagerAdapter
+    mPagerAdapter.addTab(mActionbar.newTab(),
+                         R.drawable.ic_action_seminar,
+                         R.string.Overview,
+                         CourseOverviewFragment.class,
+                         intentExtras);
+    mPagerAdapter.addTab(mActionbar.newTab(),
+                         R.drawable.ic_action_schedule,
+                         R.string.Schedule,
+                         CourseScheduleFragment.class,
+                         intentExtras);
+    mPagerAdapter.addTab(mActionbar.newTab(),
+                         R.drawable.ic_action_attendees,
+                         R.string.attendees,
+                         CourseAttendeesFragment.class,
+                         intentExtras);
+    mPagerAdapter.addTab(mActionbar.newTab(),
+                         R.drawable.ic_action_files,
+                         R.string.Documents,
+                         CourseDocumentsFragment.class,
+                         intentExtras);
 
-        // Setting Activity title
-        setTitle(getIntent()
-                .getStringExtra(CoursesContract.Columns.Courses.COURSE_TITLE));
+    // Setting Activity title
+    setTitle(getIntent().getStringExtra(CoursesContract.Columns.Courses.COURSE_TITLE));
 
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      // Respond to the action bar's Up/Home button
+      case android.R.id.home:
+        NavUtils.navigateUpFromSameTask(this);
+        return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            // Respond to the action bar's Up/Home button
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-        }
+    return super.onOptionsItemSelected(item);
+  }
 
-        return super.onOptionsItemSelected(item);
+
+  @Override
+  public void onBackPressed() {
+    if (!returnBackStackImmediate(getSupportFragmentManager())) {
+      super.onBackPressed();
     }
+  }
 
-
-    @Override
-    public void onBackPressed() {
-        if (!returnBackStackImmediate(getSupportFragmentManager())) {
-            super.onBackPressed();
-        }
-    }
-
-    // HACK: propagate back button press to child fragments.
-    // (http://android.joao.jp/2013/09/back-stack-with-nested-fragments-back.html)
-    // FIXME: Fix with either a better hack, or an official solution
-    private boolean returnBackStackImmediate(FragmentManager fm) {
-        List<Fragment> fList = fm.getFragments();
-        Fragment fActive = mPagerAdapter.getItem(mPager.getCurrentItem());
-        if (fActive instanceof CourseDocumentsFragment) {
-            if (fList != null && fList.size() > 0) {
-                for (Fragment f : fList) {
-                    if (f.getChildFragmentManager().getBackStackEntryCount() > 0) {
-                        if (f.getChildFragmentManager().popBackStackImmediate()) {
-                            return true;
-                        } else {
-                            return returnBackStackImmediate(f.getChildFragmentManager());
-                        }
-                    }
-                }
+  // HACK: propagate back button press to child fragments.
+  // (http://android.joao.jp/2013/09/back-stack-with-nested-fragments-back.html)
+  // FIXME: Fix with either a better hack, or an official solution
+  private boolean returnBackStackImmediate(FragmentManager fm) {
+    List<Fragment> fList = fm.getFragments();
+    Fragment fActive = mPagerAdapter.getItem(mPager.getCurrentItem());
+    if (fActive instanceof CourseDocumentsFragment) {
+      if (fList != null && fList.size() > 0) {
+        for (Fragment f : fList) {
+          if (f.getChildFragmentManager().getBackStackEntryCount() > 0) {
+            if (f.getChildFragmentManager().popBackStackImmediate()) {
+              return true;
+            } else {
+              return returnBackStackImmediate(f.getChildFragmentManager());
             }
+          }
         }
-        return false;
+      }
     }
+    return false;
+  }
 }
