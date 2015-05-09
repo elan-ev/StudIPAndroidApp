fb372c9bb7078ceb7286fe39ff28ffcd8e381dc9
Jörn <j.domnik@googlemail.com>
2015-05-09 15:03:31 +0200

    Use AppCompat and Toolbar Port ActionBarActivities to AppCompatActivities Replace Actionbar with Toolbar Update some formatting and copyright notices

--- StudIPAndroidApp/src/main/java/de/elanev/studip/android/app/frontend/messages/MessageComposeActivity.java
+++ StudIPAndroidApp/src/main/java/de/elanev/studip/android/app/frontend/messages/MessageComposeActivity.java
@@ -1,75 +1,61 @@
-/*******************************************************************************
- * Copyright (c) 2013 ELAN e.V.
+/*
+ * Copyright (c) 2015 ELAN e.V.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
- ******************************************************************************/
+ */
+
 package de.elanev.studip.android.app.frontend.messages;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.Window;
 
 import de.elanev.studip.android.app.R;
 
-public class MessageComposeActivity extends ActionBarActivity {
+public class MessageComposeActivity extends AppCompatActivity {
 
-    private static final String TAG = MessageComposeActivity.class.getSimpleName();
+  private static final String TAG = MessageComposeActivity.class.getSimpleName();
 
-    /*
-         * (non-Javadoc)
-         * 
-         * @see android.support.v4.app.FragmentActivity#onCreate(android.os.Bundle)
-         */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-        supportRequestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-        Bundle args = getIntent().getExtras();
+  @Override public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    // First request toolbar progrss indicator
+    supportRequestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 
-		setContentView(R.layout.content_frame);
-		setTitle(R.string.compose_message);
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    // Then set the content with toolbar
+    setContentView(R.layout.content_frame);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setHomeButtonEnabled(true);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    setTitle(R.string.compose_message);
 
-//
-//		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-//		Fragment frag = MessageComposeFragment.instantiate(this,
-//				MessageComposeFragment.class.getName());
-//		frag.setArguments(args);
-//		ft.replace(R.id.content_frame, frag, "messageComposeFragment").commit();
+    Bundle args = getIntent().getExtras();
 
-        if (savedInstanceState == null) {
-            MessageComposeFragment meesageComposeFragment = MessageComposeFragment.newInstance(args);
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.content_frame, meesageComposeFragment,
-                            MessageComposeFragment.class.getName())
-                    .commit();
+    if (savedInstanceState == null) {
+      MessageComposeFragment meesageComposeFragment = MessageComposeFragment.newInstance(args);
+      getSupportFragmentManager().beginTransaction()
+          .add(R.id.content_frame, meesageComposeFragment, MessageComposeFragment.class.getName())
+          .commit();
 
-        }
+    }
 
-	}
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.actionbarsherlock.app.SherlockFragmentActivity#onOptionsItemSelected
-	 * (com.actionbarsherlock.view.MenuItem)
-	 */
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		// Respond to the action bar's Up/Home button
-		case android.R.id.home:
-			// Since this activity can be called from different other
-			// activities, we call the back button to move back in stack history
-			onBackPressed();
-			return true;
-		}
+  @Override public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      // Respond to the action bar's Up/Home button
+      case android.R.id.home:
+        // Since this activity can be called from different other
+        // activities, we call the back button to move back in stack history
+        onBackPressed();
+        return true;
+    }
 
-		return super.onOptionsItemSelected(item);
-	}
+    return super.onOptionsItemSelected(item);
+  }
 
 }
