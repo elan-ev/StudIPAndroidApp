1d97bba36ab2cdc542c67f63095058fc44c9a7a1
joern <j.domnik@googlemail.com>
2015-07-27 16:22:06 +0200

    Redesign course overview, create name helper method, create user info layout

--- StudIPAndroidApp/src/main/java/de/elanev/studip/android/app/util/TextTools.java
+++ StudIPAndroidApp/src/main/java/de/elanev/studip/android/app/util/TextTools.java
@@ -146,4 +146,16 @@ public class TextTools {
   public static String stripImages(String content) {
     return content.replaceAll("\\<img.*?>", "");
   }
+
+  public static String createNameSting(String... parts) {
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0; i < parts.length; i++) {
+      if (i != 0) {
+        builder.append(" ");
+      }
+      builder.append(parts[i]);
+    }
+
+    return builder.toString().trim();
+  }
 }
