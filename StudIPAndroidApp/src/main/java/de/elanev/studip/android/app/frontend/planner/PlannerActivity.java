1e4f6e524b4f549adaf0d0b89ceabfd15388aca7
joern <j.domnik@googlemail.com>
2015-11-27 12:14:26 +0100

    Add option to switch between List- and Timetable view
    * Fixed duplicate Fragment adding on device rotation
    * Add OptionsMenu options for switching between List- and Timetableview
    * Implement switching logic
    * Store last used view in Prefs and reuse it

--- StudIPAndroidApp/src/main/java/de/elanev/studip/android/app/frontend/planner/PlannerActivity.java
+++ StudIPAndroidApp/src/main/java/de/elanev/studip/android/app/frontend/planner/PlannerActivity.java
@@ -12,6 +12,8 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import de.elanev.studip.android.app.MainActivity;
 import de.elanev.studip.android.app.R;
@@ -25,6 +27,36 @@ public class PlannerActivity extends MainActivity {
   public static final String PLANNER_VIEW_TIMETABLE = "planner-view-timetable";
   public static final String PLANNER_VIEW_LIST = "planner-view-list";
   private static final String PLANNER_PREFERRED_VIEW = "planner-preferred-view";
+  private static final String FRAGMENT_TAG = "planner-fragment";
+  private int mOrientation;
+
+  @Override public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.planner_activity_menu, menu);
+
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  @Override public boolean onOptionsItemSelected(MenuItem item) {
+    if (item.isCheckable() && item.isChecked()) {
+      item.setChecked(false);
+    } else {
+      item.setChecked(true);
+    }
+
+    switch (item.getItemId()) {
+      case R.id.planner_goto_today:
+        scrollToCurrentTime();
+        return true;
+      case R.id.planner_timetable:
+        shouldSwitchFragment(R.id.planner_timetable);
+        return true;
+      case R.id.planner_list:
+        shouldSwitchFragment(R.id.planner_list);
+        return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
 
   @Override protected int getCurrentNavDrawerItem() {
     return R.id.navigation_planner;
@@ -34,18 +66,21 @@ public class PlannerActivity extends MainActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    int orientation = getResources().getConfiguration().orientation;
+    mOrientation = getResources().getConfiguration().orientation;
     String preferredView = Prefs.getInstance(this)
-        .getPreferredPlannerView(orientation);
+        .getPreferredPlannerView(mOrientation);
 
-    initFragment(preferredView);
     overridePendingTransition(0, 0);
+    if (savedInstanceState == null) {
+      initFragment(preferredView);
+    }
   }
 
   private void initFragment(String preferredView) {
     Bundle args = new Bundle();
     args.putString(PLANNER_PREFERRED_VIEW, preferredView);
 
+    FragmentManager fragmentManager = getSupportFragmentManager();
     Fragment fragment;
     if (TextUtils.equals(preferredView, PLANNER_VIEW_LIST)) {
       fragment = PlannerListFragment.newInstance(args);
@@ -53,9 +88,41 @@ public class PlannerActivity extends MainActivity {
       fragment = PlannerTimetableFragment.newInstance(args);
     }
 
-    FragmentManager fragmentManager = getSupportFragmentManager();
     fragmentManager.beginTransaction()
-        .add(R.id.content_frame, fragment, "planner-fragment")
+        .add(R.id.content_frame, fragment, FRAGMENT_TAG)
+        .commit();
+  }
+
+  private void scrollToCurrentTime() {
+    FragmentManager fm = getSupportFragmentManager();
+    PlannerFragment fragment = (PlannerFragment) fm.findFragmentByTag(FRAGMENT_TAG);
+
+    if (fragment != null) {
+      fragment.scrollToCurrentTime();
+    }
+  }
+
+  private void shouldSwitchFragment(int plannerView) {
+    FragmentManager fm = getSupportFragmentManager();
+    Fragment fragment = fm.findFragmentByTag(FRAGMENT_TAG);
+    switch (plannerView) {
+      case R.id.planner_list:
+        Prefs.getInstance(this)
+            .setPlannerPreferredView(mOrientation, PLANNER_VIEW_LIST);
+        if (fragment == null || !(fragment instanceof PlannerListFragment)) {
+          fragment = PlannerListFragment.newInstance(new Bundle());
+        }
+        break;
+      case R.id.planner_timetable:
+        Prefs.getInstance(this)
+            .setPlannerPreferredView(mOrientation, PLANNER_VIEW_TIMETABLE);
+        if (fragment == null || !(fragment instanceof PlannerTimetableFragment)) {
+          fragment = PlannerTimetableFragment.newInstance(new Bundle());
+        }
+        break;
+    }
+    fm.beginTransaction()
+        .replace(R.id.content_frame, fragment, FRAGMENT_TAG)
         .commit();
   }
 }
