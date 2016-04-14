e93ca6dbd7c9a1dc4ba4c37cb62425a60a63e0c1
joern <j.domnik@googlemail.com>
2016-04-14 11:36:50 +0200

    Following the androdi naming conventions, renamed app module to app

---
+++ app/src/productionDebug/java/de/elanev/studip/android/app/util/ServerData.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2014 ELAN e.V.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ */
+package de.elanev.studip.android.app.util;
+
+/**
+ * @author Jörn
+ */
+public final class ServerData {
+  public final static String serverJson =
+      "{"
+          + "  \"servers\": ["
+	  + "    {"
+          + "      \"name\": \"Universität Osnabrück\","
+          + "      \"consumer_key\": \"e4450a85a3d48162ab5fcae59c219992052600ca4\","
+          + "      \"consumer_secret\": \"0d713fcc36bcf5a5bdf0043745c2dd48\","
+          + "      \"base_url\": \"https://studip.uni-osnabrueck.de/plugins.php/restipplugin\","
+          + "      \"contact_email\": \"studipmobil@elan-ev.de\""
+          + "    },"
+	  + "    {"
+          + "      \"name\": \"Test Stud.IP\","
+          + "      \"consumer_key\": \"e4450a85a3d48162ab5fcae59c219992052600ca4\","
+          + "      \"consumer_secret\": \"0d713fcc36bcf5a5bdf0043745c2dd48\","
+          + "      \"base_url\": \"https://studip-test.uni-osnabrueck.de/plugins.php/restipplugin\","
+          + "      \"contact_email\": \"root@studip.de\""
+          + "    },"
+          + "    {"
+          + "      \"name\": \"Test Stud.IP New API\","
+          + "      \"consumer_key\": \"9c748883b5477f8fe1d1e157b91e824b054ede0e0\","
+          + "      \"consumer_secret\": \"7ad3eed51a74616372c04259fc6124a7\","
+          + "      \"base_url\": \"http://studip-test.uni-osnabrueck.de/api.php\","
+          + "      \"contact_email\": \"root@studip.de\""
+          + "    },"
+	  + "    {"
+          + "      \"name\": \"Testumgebung\","
+          + "      \"consumer_key\": \"4cc7aa80eb6dc80cdd8658bf61298f44053ce7ef8\","
+          + "      \"consumer_secret\": \"5ef6def72c81286f4231257aff1ffd03\","
+          + "      \"base_url\": \"http://vm396.rz.uni-osnabrueck.de/studip/aklassen/studip2.5/plugins.php/restipplugin\","
+          + "      \"contact_email\": \"root@studip.de\""
+          + "    },"	    
+          + "    {"
+          + "      \"name\": \"Stud.IP Developer Server\","
+          + "      \"consumer_key\": \"b713cfcc739715c187fb3ff88e8f68d30524bfdd5\","
+          + "      \"consumer_secret\": \"2b291630016fd00a232e2e1aa9fec5b7\","
+          + "      \"base_url\": \"http://develop.studip.de/studip/plugins.php/restipplugin\","
+          + "      \"contact_email\": \"studipmobil@elan-ev.de\""
+          + "    }"
+          + "  ]"
+          + "}";
+}
