04107011d64dda25ee1e5d6fc667e3fc19a1f074
joern <j.domnik@googlemail.com>
2016-08-20 11:34:19 +0200

    Add unit tests for the use cases

---
+++ app/src/test/java/de/elanev/studip/android/app/news/domain/GetNewsDetailsTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2016 ELAN e.V.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ */
+
+package de.elanev.studip.android.app.news.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.UUID;
+
+import de.elanev.studip.android.app.base.domain.executor.PostExecutionThread;
+import de.elanev.studip.android.app.base.domain.executor.ThreadExecutor;
+import rx.Scheduler;
+import rx.schedulers.Schedulers;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.verifyZeroInteractions;
+
+/**
+ * @author joern
+ */
+public class GetNewsDetailsTest {
+  private static final String FAKE_UID = UUID.randomUUID()
+      .toString();
+
+  private GetNewsDetails getNewsDetails;
+
+  @Mock private NewsRepository mockNewsRepository;
+  @Mock private ThreadExecutor mockThreadExecutor;
+  @Mock private PostExecutionThread mockPostExecutionThread;
+
+  @Before public void setUp() {
+    MockitoAnnotations.initMocks(this);
+
+    getNewsDetails = new GetNewsDetails(FAKE_UID, mockNewsRepository, mockThreadExecutor,
+        mockPostExecutionThread);
+  }
+
+  @Test public void buildUseCaseObservable() throws Exception {
+    given(mockThreadExecutor.getScheduler()).willReturn(Schedulers.immediate());
+    given(mockPostExecutionThread.getScheduler()).willReturn(Schedulers.immediate());
+
+    getNewsDetails.buildUseCaseObservable();
+
+    verify(mockNewsRepository).newsItem(FAKE_UID);
+    verifyNoMoreInteractions(mockNewsRepository);
+
+    verifyZeroInteractions(mockThreadExecutor);
+    verifyZeroInteractions(mockPostExecutionThread);
+  }
+
+}
