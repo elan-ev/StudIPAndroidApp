132e8f6a540017effaf2238de7f6166c09de3b59
joern <j.domnik@googlemail.com>
2017-01-13 13:19:02 +0100

    Fix not loading planner events in timetable view

--- app/src/main/java/de/elanev/studip/android/app/planner/presentation/presenter/PlannerTimetablePresenter.java
+++ app/src/main/java/de/elanev/studip/android/app/planner/presentation/presenter/PlannerTimetablePresenter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 ELAN e.V.
+ * Copyright (c) 2017 ELAN e.V.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
@@ -13,13 +13,13 @@ import java.util.List;
 import javax.inject.Inject;
 
 import de.elanev.studip.android.app.base.BaseRxLcePresenter;
-import de.elanev.studip.android.app.base.DefaultSubscriber;
 import de.elanev.studip.android.app.base.UseCase;
 import de.elanev.studip.android.app.base.internal.di.PerActivity;
-import de.elanev.studip.android.app.planner.domain.Event;
+import de.elanev.studip.android.app.planner.domain.GetEventsList;
 import de.elanev.studip.android.app.planner.presentation.mapper.PlanerModelDataMapper;
 import de.elanev.studip.android.app.planner.presentation.model.PlanerEventModel;
 import de.elanev.studip.android.app.planner.presentation.view.PlannerTimetableView;
+import rx.Subscriber;
 import timber.log.Timber;
 
 /**
@@ -30,40 +30,34 @@ public class PlannerTimetablePresenter extends
     BaseRxLcePresenter<PlannerTimetableView, List<PlanerEventModel>> {
 
   private final PlanerModelDataMapper planerModelDataMapper;
-  private final UseCase getEventsList;
+  private final GetEventsList getEventsList;
 
   @Inject public PlannerTimetablePresenter(UseCase getEventsList,
       PlanerModelDataMapper planerModelDataMapper) {
-    this.getEventsList = getEventsList;
+    this.getEventsList = (GetEventsList) getEventsList;
     this.planerModelDataMapper = planerModelDataMapper;
   }
 
   public void loadData(boolean ptr) {
-    this.getEventsList.execute(new PlannerTimetableSubscriber(ptr));
+    this.getEventsList.get(ptr)
+        .map(planerModelDataMapper::transform)
+        .subscribe(new Subscriber<List<PlanerEventModel>>() {
+          @Override public void onCompleted() {
+            PlannerTimetablePresenter.this.onCompleted();
+          }
+
+          @Override public void onError(Throwable e) {
+            Timber.e(e, e.getLocalizedMessage());
+            PlannerTimetablePresenter.this.onError(e, ptr);
+          }
+
+          @Override public void onNext(List<PlanerEventModel> planerEventModel) {
+            PlannerTimetablePresenter.this.onNext(planerEventModel);
+          }
+        });
   }
 
   @Override protected void unsubscribe() {
     this.getEventsList.unsubscribe();
-  }
-
-  private final class PlannerTimetableSubscriber extends DefaultSubscriber<List<Event>> {
-
-    public PlannerTimetableSubscriber(boolean ptr) {
-      super(ptr);
-    }
-
-    @Override public void onCompleted() {
-      PlannerTimetablePresenter.this.onCompleted();
-    }
-
-    @Override public void onError(Throwable e) {
-      Timber.e(e, e.getLocalizedMessage());
-      PlannerTimetablePresenter.this.onError(e, this.isPullToRefresh());
-    }
-
-    @Override public void onNext(List<Event> events) {
-      PlannerTimetablePresenter.this.onNext(
-          PlannerTimetablePresenter.this.planerModelDataMapper.transform(events));
-    }
   }
 }
