74155845bdbedee82cf750745b23e441f4c0c085
Jörn <j.domnik@googlemail.com>
2017-02-25 16:56:35 +0100

    Fix broken CI
    * introduced staging build type
    * add dummy ServerData
    * add dummy debug keystore

---
+++ app/src/staging/java/de/elanev/studip/android/app/StudIPApplication.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2017 ELAN e.V.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ */
+
+package de.elanev.studip.android.app;
+
+import timber.log.Timber;
+
+/**
+ * Debug build variant Application class
+ *
+ * @author joern
+ */
+public class StudIPApplication extends AbstractStudIPApplication {
+  @Override public void onCreate() {
+    super.onCreate();
+
+    Timber.plant(new Timber.DebugTree() {
+      @Override protected String createStackElementTag(StackTraceElement element) {
+        return super.createStackElementTag(element) + ":" + element.getLineNumber();
+      }
+    });
+  }
+}
