34e957fc76eaf22e019cf385092865b8e5af3a41
joern <j.domnik@googlemail.com>
2017-03-08 19:26:35 +0100

    Fix duplicate news entries

--- app/src/main/java/de/elanev/studip/android/app/news/domain/GetNewsList.java
+++ app/src/main/java/de/elanev/studip/android/app/news/domain/GetNewsList.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 ELAN e.V.
+ * Copyright (c) 2017 ELAN e.V.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
@@ -41,12 +41,14 @@ public class GetNewsList extends UseCase<List<NewsItem>> {
                 forceUpdate))));
 
     return Observable.zip(mRepository.newsList(forceUpdate), coursesObs, (news, courses) -> {
-
       ArrayList<NewsItem> newsItems = new ArrayList<>(news.size() + courses.size());
       newsItems.addAll(news);
       newsItems.addAll(courses);
 
       return newsItems;
-    });
+    })
+        .flatMap(newsItems -> Observable.from(newsItems)
+            .distinct()
+            .toList());
   }
 }
--- app/src/main/java/de/elanev/studip/android/app/news/domain/NewsItem.java
+++ app/src/main/java/de/elanev/studip/android/app/news/domain/NewsItem.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 ELAN e.V.
+ * Copyright (c) 2017 ELAN e.V.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
@@ -24,9 +24,39 @@ public class NewsItem {
   private String mRange;
   private Course course;
 
+  public NewsItem(String newsId, String title, Long date, String body, User author, String mRange,
+      Course course) {
+    this.newsId = newsId;
+    this.title = title;
+    this.date = date;
+    this.body = body;
+    this.author = author;
+    this.mRange = mRange;
+    this.course = course;
+  }
   public NewsItem() {}
 
   public NewsItem(String newsId) {this.newsId = newsId;}
+
+  @Override public int hashCode() {
+    return newsId.hashCode();
+  }
+
+  @Override public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    NewsItem newsItem = (NewsItem) o;
+
+    return newsId.equals(newsItem.newsId);
+
+  }
+
+  @Override public String toString() {
+    return "NewsItem{" + "newsId='" + newsId + '\'' + ", title='" + title + '\'' + ", date=" + date
+        + ", body='" + body + '\'' + ", author=" + author + ", mRange='" + mRange + '\''
+        + ", course=" + course + '}';
+  }
 
   public String getNewsId() {
     return newsId;
@@ -74,12 +104,6 @@ public class NewsItem {
 
   public void setCourse(Course course) {
     this.course = course;
-  }
-
-  @Override public String toString() {
-    return "NewsItem{" + "newsId='" + newsId + '\'' + ", title='" + title + '\'' + ", date=" + date
-        + ", body='" + body + '\'' + ", author=" + author + ", mRange='" + mRange + '\''
-        + ", course=" + course + '}';
   }
 
   public String getRange() {
